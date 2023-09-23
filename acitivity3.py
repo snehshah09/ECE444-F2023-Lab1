0e8730942c1409745d2e3fc26d0c0d02832d1e95
Sneh Shah <sneh@snehs-mbp.home>
2023-09-22 23:13:37 -0400

    Activity 3 completed

--- acitivity3.py
+++ acitivity3.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template 
-from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 @app.route('/') 
 def index():
-    return render_template("index.html")
+        return '<h1>Hello World!</h1>'
 
 @app.route('/user/<name>') 
 def user(name):
-    return render_template('user.html', name=name, current_time=datetime.utcnow())
+    return render_template('user.html', name=name, currentTime=datetime.utcnow())
