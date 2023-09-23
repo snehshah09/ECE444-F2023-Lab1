fd88ec7f20eb04f597aeeaa5651f334b5a087d88
Sneh Shah <sneh@snehs-mbp.home>
2023-09-23 14:21:22 -0400

    Completed 4 Part 2 with email validation and messages

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for, flash 
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField 
+from wtforms import StringField, SubmitField, EmailField 
 from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
 from datetime import datetime
@@ -14,7 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) 
-    Email = StringField('What is your uoft email?', validators=[DataRequired()]) 
+    Email = EmailField('What is your uoft email?', validators=[DataRequired(), Email()]) 
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,11 +23,19 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
          old_name = session.get('name')
+         old_email = session.get('email')
          if old_name is not None and old_name != form.name.data:
              flash('Looks like you have changed your name!')
          session['name'] = form.name.data
+         if old_email is not None and old_email != form.Email.data:
+             flash('Looks like you have changed your email!')
+         session['email'] = form.Email.data
+
+         if session.get('email').find('utoronto') == -1:
+             session['email'] = 'wrong-email'
+             
          return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get("email"))
 
 @app.route('/user/<name>') 
 def user(name):
